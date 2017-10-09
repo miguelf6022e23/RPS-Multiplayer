1d4c0499c8e43725f1a06726e26f5b7aac3f6723
miguelf6022e23 <miguelf6.022e23@gmail.com>
2017-10-09 00:38:02 -0400

    Graphic Design Done

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+console.log("hi")
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-RL1NtIOV6auaJWFhhUVibp3b1hD1efs",
+    authDomain: "rpsm-194ce.firebaseapp.com",
+    databaseURL: "https://rpsm-194ce.firebaseio.com",
+    projectId: "rpsm-194ce",
+    storageBucket: "",
+    messagingSenderId: "69716634755"
+  };
+  firebase.initializeApp(config);
